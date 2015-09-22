f39bc30c7f8708acbfbe097bb2d7ad2f9de96a6a
zhang.la <zhang.la@Ctrip.com>
2015-09-22 18:29:05 +0800

    add container

---
+++ MyBusiness/app/src/main/java/com/example/business/mybusiness/view/widget/FragmentContainer.java
@@ -0,0 +1,22 @@
+package com.example.business.mybusiness.view.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by zhang.la on 2015/9/22.
+ */
+public class FragmentContainer extends LinearLayout {
+    public FragmentContainer(Context context) {
+        super(context);
+    }
+
+    public FragmentContainer(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FragmentContainer(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
